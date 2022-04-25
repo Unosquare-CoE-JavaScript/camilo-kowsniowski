ef1846f04215270e16402efd695ccdbfb380f50d
camilo kosniowski <camilo.kosniowski@unosquare.com>
2022-04-25 10:25:14 -0500

    complete the section 6

---
+++ vue/Section-6/Exercise1/src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <button type="button" @click="onClickAge">age update</button>
+  <p>the user is {{ age }} years old</p>
+  <p>{{ ageDoubled }}</p>
+</template>
+<script>
+export default {
+  name: 'user',
+  //   validating values
+  props: {
+    age: {
+      type: Number,
+      //   required: true,
+      //   default: 20
+      validator(value) {
+        return value < 130;
+      }
+    }
+  },
+  emits: ['age-change'],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    }
+  },
+  methods: {
+    onClickAge() {
+      this.$emit('age-Change', 3);
+    }
+  }
+};
+</script>
