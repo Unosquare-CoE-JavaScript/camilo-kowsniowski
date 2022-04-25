6637fc9684211c9eee66e92436f275ca72a9510b
camilo kosniowski <camilo.kosniowski@unosquare.com>
2022-04-24 23:04:47 -0500

    adding serction 6

---
+++ vue/Section-6/hello/src/components/Greeting.vue
@@ -0,0 +1,23 @@
+<template>
+  <p v-if="age > 25">{{ msg }}</p>
+  <p v-else>you must be 25 years or older to view this message</p>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+  props: ['age'],
+  data() {
+    return {
+      msg: 'hello world'
+    };
+  }
+};
+</script>
+// using sass
+<style scoped lang="scss">
+$color: red;
+p {
+  color: $color;
+}
+</style>
